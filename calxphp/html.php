b2b6d97bda8df04724d61a3df16e03ddd688f710
nicolairygh <nicolai@rygh.no>
2020-03-23 00:06:00 +0100

    Create html.php

---
+++ calxphp/html.php
@@ -0,0 +1,42 @@
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Calx</title>
+<link rel="shortcut icon" href="https://calx.no/fx/ryghweb.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="https://calx.no/fx/calx.png" />
+<link rel="apple-touch-icon-precomposed" href="https://calx.no/fx/calx.png" />
+</head>
+<body link="#000000" vlink="#000000" alink="#000000" bgcolor="#C0C0C0" topmargin="0" bottommargin="0" marginwidth="0" marginheight="0" style="background-image: url('https://calx.no/fx/bg.jpg')">
+<style type="text/css">
+body, td {
+font-size : 10px; 
+font-family : verdana;
+color : #333333; 
+} 
+.a {
+color : #333333;
+text-decoration : none;
+}
+
+.tile-badge{position:absolute;right:0;bottom:0}div.footerNode{margin:0;float:right}div.footerNode a,div.footerNode span{color:#fff;font-size:12px;line-height:28px;white-space:nowrap;display:inline-block;margin-left:8px;margin-right:8px}h3,;
+}
+.style1 {
+	color: #FF0000;
+}
+
+</style>
+<script type="text/javascript">
+<!--
+if (window != top)
+top.location.href = location.href;
+// -->
+</script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-106571166-1', 'auto');
+ga('send', 'pageview');
+
+</script>

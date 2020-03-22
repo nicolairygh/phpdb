f7349d9b8180dc659221760ce76fe7887c8eb92e
Nicolai Rygh <nicolai@rygh.no>
2020-03-23 00:15:11 +0100

    Delete about.php

--- calxphp/calxphp/about.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-include 'html.php';
-echo "<b>RyghCalx</b><br>Du er logget inn men det har skjedd en feil ved innlastning av din kalender.<br>Hvis dette gjentar seg ta kontakt med admin - pr¯v pÂ nytt <a href='logout.php'>her</a><br>";
-echo $_COOKIE["cookiejar"];
-?>
